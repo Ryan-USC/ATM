cc7919f8d182f9afdd5d99b2bce938af5ee69038
Yuan Lu <70478677+Ryan-USC@users.noreply.github.com>
2022-04-17 18:56:09 -0700

    Add files via upload

---
+++ ATMMachine/src/ATM.sql
@@ -0,0 +1,10 @@
+
+create table accounts(
+pin int(4) not null,
+balance int not null,
+primary key(pin));
+
+INSERT INTO accounts VALUES(1234, 10000);
+INSERT INTO accounts VALUES(1111, 1000);
+INSERT INTO accounts VALUES(2222, 100);
+INSERT INTO accounts VALUES(3333, 5000);
